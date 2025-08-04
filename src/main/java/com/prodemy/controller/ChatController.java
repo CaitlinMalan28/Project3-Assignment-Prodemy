38a340fdf187ff586dbcb03fafbe6f1e826c705e
gabrielkiewietz <kiewietzg16@gmail.com>
2025-08-04 15:52:39 +0200

    some code

--- src/main/java/com/prodemy/controller/ChatController.java
+++ src/main/java/com/prodemy/controller/ChatController.java
@@ -1,5 +1,6 @@
 package com.prodemy.controller;
 
+import com.prodemy.model.DiscussionMessage;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
